da8852ee29b6ada7251e28a2bad4c3123f6e4fd3
Leonardo Duarte <61410466+leo01eidt@users.noreply.github.com>
2021-11-29 15:22:42 -0300

    Add files via upload

---
+++ Text-to-Speech.py
@@ -0,0 +1,60 @@
+from tkinter import *
+from tkinter import font
+from gtts import gTTS
+from playsound import playsound
+
+#Inicialização da GUI feita em Tkinter.
+
+root = Tk()
+root.geometry('350x350')
+root.resizable(0, 0)
+root.config(bg = 'black')
+root.title("Leleo's Tabajara - Texto para Fala")
+
+
+
+#Cabeçario
+
+Label(root, text = "Texto para Fala", font = "Arial 20 bold", bg = 'white smoke').pack()
+Label(root, text = "Leleo's Tabajara - Texto para Fala ", font = "Arial 15 bold", bg = "White smoke").pack(side = BOTTOM)
+
+
+#Label 
+
+Label(root, text = "Digite seu texto. ", font = "Arial 15 bold", bg = 'white smoke').pack(side = BOTTOM)
+
+
+#Variável do Texto
+
+Msg = StringVar()
+
+#Entrada
+
+entry_field = Entry(root, textvariable = Msg, width='50')
+entry_field.place(x=20, y=100)
+
+
+# Definir a função
+
+def Text_To_Speech():
+    Message = entry_field.get()
+    speech = gTTS(text=Message)
+    speech.save('LeleosTabajara.mp3')
+    playsound('LeleosTabajara.mp3')
+def Exit():
+    root.destroy()
+def Reset():
+    Msg.set("")
+
+
+#Butão hehe
+
+#Button
+Button(root, text = "PLAY" , font = 'arial 15 bold', command = Text_To_Speech, width = 4).place(x = 25, y = 140)
+Button(root,text = 'EXIT',font = 'arial 15 bold' , command = Exit, bg = 'OrangeRed1').place(x = 100,y = 140)
+Button(root, text = 'RESET', font='arial 15 bold', command = Reset).place(x = 175 , y = 140)
+
+#Loop para rodar o programa
+
+root.mainloop()
+
